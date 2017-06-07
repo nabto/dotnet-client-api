0678215572702bf09ddc3cc6356c138901948ab9
Michael Madsen <mkm@nabto.com>
2017-06-07 16:42:35 +0200

    added test

---
+++ sdk/src/Nabto.Client.Tests/NabtoClientTest.cs
@@ -0,0 +1,12 @@
+using Nabto.Client;
+using NUnit.Framework;
+
+namespace Nabto.Client.Tests {
+    public class NabtoClientTest {
+        [Test]
+        public void Constructor_Test()
+        {
+            NabtoClient nabto = new NabtoClient();
+        }
+    }
+}
