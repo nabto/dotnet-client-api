7f139ef41972ae6c9c3a14a8648ea1a5c0fc3b0d
Michael Madsen <mkm@nabto.com>
2017-07-21 13:09:11 +0200

    split docker build files into two

--- docker-build-api.sh
+++ docker-build-api.sh
@@ -5,4 +5,3 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 chmod +x $DIR/build-api.sh
 
 docker run -i -t -v $DIR:/source microsoft/dotnet /source/build-api.sh
-docker run -i -t -v $DIR:/source microsoft/dotnet /source/build-examples.sh
