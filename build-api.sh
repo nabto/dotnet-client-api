656661860e4dd82ec8686d148bc027b5121c12c7
Michael Madsen <mkm@nabto.com>
2017-07-21 13:06:32 +0200

    better error handling

--- build-api.sh
+++ build-api.sh
@@ -8,24 +8,24 @@ cd $DIR/src/NabtoClient
 
 mkdir -p $DIR/artifacts
 
-dotnet restore
-dotnet build -c Release
-dotnet pack -o ../../artifacts -c Release
+dotnet restore || exit 1
+dotnet build -c Release || exit 1
+dotnet pack -o ../../artifacts -c Release || exit 1
 
 ## run tests which does not rely on the build NabtoClient nuget package
 
-cd $DIR/src/NabtoClientDemo
+cd $DIR/src/NabtoClientDemo || exit 1
 
 # build dotnet multi-arch app to test that the api can be used
-dotnet restore
-dotnet build
-dotnet pack
-dotnet publish -f netcoreapp1.1
+dotnet restore || exit 1
+dotnet build || exit 1
+dotnet pack || exit 1
+dotnet publish -f netcoreapp1.1 || exit 1
 
 # build dotnet single-arch app to test that the api can be used
-dotnet restore -r debian.8-x64
-dotnet build -r debian.8-x64
-dotnet publish -r debian.8-x64 -f netcoreapp1.1
+dotnet restore -r debian.8-x64 || exit 1
+dotnet build -r debian.8-x64 || exit 1
+dotnet publish -r debian.8-x64 -f netcoreapp1.1 || exit 1
 
 
 
--- build-examples.sh
+++ build-examples.sh
@@ -2,27 +2,27 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd $DIR/examples/csharp/FetchUrl
+cd $DIR/examples/csharp/FetchUrl || exit 1
 
-dotnet restore
-dotnet build
+dotnet restore || exit 1
+dotnet build || exit 1
 
-cd $DIR/examples/csharp/RPC
+cd $DIR/examples/csharp/RPC || exit 1
 
-dotnet restore
-dotnet build
+dotnet restore || exit 1
+dotnet build || exit 1
 
-cd $DIR/examples/csharp/StreamEcho
+cd $DIR/examples/csharp/StreamEcho || exit 1
 
-dotnet restore
-dotnet build
+dotnet restore || exit 1
+dotnet build || exit 1
 
-cd $DIR/examples/csharp/StreamTerminal
+cd $DIR/examples/csharp/StreamTerminal || exit 1
 
-dotnet restore
-dotnet build
+dotnet restore || exit 1
+dotnet build || exit 1
 
-cd $DIR/examples/csharp/Tunnel
+cd $DIR/examples/csharp/Tunnel || exit 1
 
-dotnet restore
-dotnet build
+dotnet restore || exit 1
+dotnet build || exit 1
